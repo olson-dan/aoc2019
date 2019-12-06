2fa46f49981c3c3f612d51f56b819558a7408ce3
olson-dan <olson-dan@users.noreply.github.com>
2019-12-05 21:42:24 -0800

    Add day 5 and 6

--- src/lib.rs
+++ src/lib.rs
@@ -272,4 +272,225 @@ pub fn solve_day4_part2(_input: &[usize]) -> usize {
     count
 }
 
+/*
+fn fetch1(v: &[isize], ip: usize, inst: isize) -> isize {
+    let x = v[ip + 1];
+    let x = match (inst / 100) % 10 {
+        0 => v[x as usize],
+        1 => x,
+        _ => unimplemented!(),
+    };
+    x
+}
+
+fn fetch2(v: &[isize], ip: usize, inst: isize) -> (isize, isize) {
+    let x = fetch1(v, ip, inst);
+    let y = fetch1(v, ip + 1, inst / 10);
+    (x, y)
+}
+
+fn fetch3(v: &[isize], ip: usize, inst: isize) -> (isize, isize, isize) {
+    let x = fetch1(v, ip, inst);
+    let y = fetch1(v, ip + 1, inst / 10);
+    let z = fetch1(v, ip + 2, inst / 100);
+    (x, y, z)
+}
+*/
+
+fn day5(input: &[isize], inp: isize) -> Vec<isize> {
+    let mut out = Vec::new();
+    let mut i = 0usize;
+    let mut v = input.to_vec();
+    loop {
+        match v[i] {
+            99 => break,
+            a if a % 100 == 1 => {
+                let (x, y, z) = (v[i + 1], v[i + 2], v[i + 3]);
+                let mode0 = (a / 100) % 10;
+                let mode1 = (a / 1000) % 10;
+                let x = if mode0 == 1 { x } else { v[x as usize] };
+                let y = if mode1 == 1 { y } else { v[y as usize] };
+                v[z as usize] = x + y;
+                i += 4;
+            }
+            a if a % 100 == 2 => {
+                let (x, y, z) = (v[i + 1], v[i + 2], v[i + 3]);
+                let mode0 = (a / 100) % 10;
+                let mode1 = (a / 1000) % 10;
+                let x = if mode0 == 1 { x } else { v[x as usize] };
+                let y = if mode1 == 1 { y } else { v[y as usize] };
+                v[z as usize] = x * y;
+                i += 4;
+            }
+            a if a % 100 == 3 => {
+                let mode0 = (a / 100) % 10;
+                assert_eq!(mode0, 0);
+                let x = v[i + 1];
+                v[x as usize] = inp;
+                i += 2;
+            }
+            a if a % 100 == 4 => {
+                let mode0 = (a / 100) % 10;
+                let x = v[i + 1];
+                if mode0 == 1 {
+                    out.push(x);
+                } else {
+                    out.push(v[x as usize]);
+                }
+                i += 2;
+            }
+            a if a % 100 == 5 => {
+                let (x, y) = (v[i + 1], v[i + 2]);
+                let mode0 = (a / 100) % 10;
+                let mode1 = (a / 1000) % 10;
+                let x = if mode0 == 1 { x } else { v[x as usize] };
+                let y = if mode1 == 1 { y } else { v[y as usize] };
+                if x != 0 {
+                    i = y as usize;
+                } else {
+                    i += 3;
+                }
+            }
+            a if a % 100 == 6 => {
+                let (x, y) = (v[i + 1], v[i + 2]);
+                let mode0 = (a / 100) % 10;
+                let mode1 = (a / 1000) % 10;
+                let x = if mode0 == 1 { x } else { v[x as usize] };
+                let y = if mode1 == 1 { y } else { v[y as usize] };
+                if x == 0 {
+                    i = y as usize;
+                } else {
+                    i += 3;
+                }
+            }
+            a if a % 100 == 7 => {
+                let (x, y, z) = (v[i + 1], v[i + 2], v[i + 3]);
+                let mode0 = (a / 100) % 10;
+                let mode1 = (a / 1000) % 10;
+                let x = if mode0 == 1 { x } else { v[x as usize] };
+                let y = if mode1 == 1 { y } else { v[y as usize] };
+                if x < y {
+                    v[z as usize] = 1;
+                } else {
+                    v[z as usize] = 0;
+                }
+                i += 4;
+            }
+            a if a % 100 == 8 => {
+                let (x, y, z) = (v[i + 1], v[i + 2], v[i + 3]);
+                let mode0 = (a / 100) % 10;
+                let mode1 = (a / 1000) % 10;
+                let x = if mode0 == 1 { x } else { v[x as usize] };
+                let y = if mode1 == 1 { y } else { v[y as usize] };
+                if x == y {
+                    v[z as usize] = 1;
+                } else {
+                    v[z as usize] = 0;
+                }
+                i += 4;
+            }
+            _ => unimplemented!(),
+        }
+    }
+    out
+}
+#[aoc_generator(day5)]
+pub fn input_generator_day5(input: &str) -> Vec<isize> {
+    input
+        .split(',')
+        .map(|x| x.trim().parse().unwrap())
+        .collect()
+}
+
+#[aoc(day5, part1)]
+pub fn solve_day5_part1(input: &[isize]) -> isize {
+    let ret = day5(input, 1);
+    *ret.last().unwrap()
+}
+
+#[aoc(day5, part2)]
+pub fn solve_day5_part2(input: &[isize]) -> isize {
+    let ret = day5(input, 5);
+    *ret.last().unwrap()
+}
+
+#[aoc_generator(day6)]
+pub fn input_generator_day6(input: &str) -> Vec<(String, String)> {
+    let mut y = Vec::new();
+    for l in input.lines() {
+        let x: Vec<&str> = l.splitn(2, ')').collect();
+        y.push((x[0].to_string(), x[1].to_string()));
+    }
+    y
+}
+
+fn count_orbits(map: &[(String, String)], this: &str) -> usize {
+    let mut count = 0;
+    for (x, y) in map {
+        if y == this {
+            count += 1 + count_orbits(map, x);
+        }
+    }
+    count
+}
+
+#[aoc(day6, part1)]
+pub fn solve_day6_part1(input: &[(String, String)]) -> usize {
+    let mut set: HashSet<String> = HashSet::new();
+    for (_, y) in input {
+        set.insert(y.clone());
+    }
+    let mut orbits = 0;
+    for k in &set {
+        orbits += count_orbits(input, k);
+    }
+    orbits
+}
+
+fn chain_orbits(map: &[(String, String)], this: &str) -> Vec<String> {
+    let mut chain = Vec::new();
+    for (x, y) in map {
+        if y == this {
+            chain.push(y.to_string());
+            chain.extend(chain_orbits(map, x));
+        }
+    }
+    chain
+}
+
+#[aoc(day6, part2)]
+pub fn solve_day6_part2(input: &[(String, String)]) -> usize {
+    let you = chain_orbits(input, "YOU");
+    let san = chain_orbits(input, "SAN");
+    let mut set = HashSet::new();
+    for x in you.iter().chain(san.iter()) {
+        set.insert(x);
+    }
+    let mut answer = usize::max_value();
+    for k in set {
+        if let Some((i, _)) = you.iter().enumerate().find(|(_, b)| *b == k) {
+            if let Some((j, _)) = san.iter().enumerate().find(|(_, b)| *b == k) {
+                answer = std::cmp::min(answer, i + j);
+            }
+        }
+    }
+    answer - 2
+}
+
+#[test]
+fn test_day6() {
+    assert_eq!(
+        42,
+        solve_day6_part1(&input_generator_day6(
+            "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L"
+        ))
+    );
+    assert_eq!(
+        4,
+        solve_day6_part2(&input_generator_day6(
+            "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN"
+        ))
+    );
+}
+
 aoc_lib! { year = 2019 }
